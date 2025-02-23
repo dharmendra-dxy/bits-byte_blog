6396c9ae8b8c7f5a59f842044def3534e26c2774
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-02-23 15:58:40 +0530

    ArticlePage ui abd data loaded from database

--- components/articles/ArticlePage.tsx
+++ components/articles/ArticlePage.tsx
@@ -2,6 +2,7 @@ import type { Prisma } from '@prisma/client'
 import { Avatar, AvatarFallback } from '@radix-ui/react-avatar'
 import React from 'react'
 import { AvatarImage } from '../ui/avatar'
+import Image from 'next/image'
 
 type articlePageTypes = {
     article : Prisma.ArticlesGetPayload<{
@@ -25,10 +26,10 @@ const ArticlePage:React.FC<articlePageTypes> = ({article}) => {
 
             <article className='mx-auto max-w-3xl'>
                 {/*  article header */}
-                <header className='mb-12'>
+                <header className='mb-10'>
 
                     <div className='flex flex-wrap gap-2 mb-4'>
-                        <span className='text-sm bg-gray-200 px-4 py-1 rounded-full hover:bg-gray-300'>
+                        <span className='text-sm bg-gray-200 px-4 py-1 rounded-full hover:bg-gray-300 dark:text-black'>
                             {article.category}
                         </span>
                     </div>
@@ -37,7 +38,7 @@ const ArticlePage:React.FC<articlePageTypes> = ({article}) => {
                         {article.title}
                     </h1>
 
-                    <div className='flex items-center gap-4 w-full'>
+                    <div className='flex items-center gap-4 w-full border-b border-gray-500 pb-4'>
                         <Avatar className='bg-gray-200 dark:bg-gray-700 h-9 w-9 flex items-center justify-center cursor-pointer rounded-full'>
                             <AvatarImage src={article.author.imageUrl || ""}/>
                             <AvatarFallback>{article.author.name[0]}</AvatarFallback>
@@ -47,6 +48,7 @@ const ArticlePage:React.FC<articlePageTypes> = ({article}) => {
                                 {article.author.name}
                             </p>
                             <p className=' text-sm text-gray-700 dark:text-gray-300'>
+                                {article.createdAt.toDateString()} , {" "}
                                 {Math.floor(article.content.length/100)} min to read
                             </p>
                         </div>
@@ -54,13 +56,25 @@ const ArticlePage:React.FC<articlePageTypes> = ({article}) => {
 
                 </header>
 
+                <div className='mb-10 mx-auto'>
+                    <Image
+                        src={article.featuredImage || ""}
+                        alt='article image'
+                        width={0}
+                        height={0}
+                        sizes="100vw"
+                        style={{width: "100%", height:'auto'}}
+                    />
+                </div>
+
                 {/* article content: */}
                 <section 
-                className='mb-12 max-w-none'
-                dangerouslySetInnerHTML={{__html:<h1>Hello</h1>}}
+                className='mb-12 max-w-none border-b border-gray-500 pb-4'
+                dangerouslySetInnerHTML={{__html:article.content}}
                 >
-                    {article.content}
                 </section>
+
+                {/* Like button: */}
 
             </article>
 
