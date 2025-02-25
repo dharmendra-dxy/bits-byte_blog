8e0871824ab171bf7cc714f3b10a7416716e8127
Dharmendra Yadav <yaduvanshi.dhanno@gmail.com>
2025-02-25 23:54:44 +0530

    deploy fix 1: defined but never used

--- components/home/Footer.tsx
+++ components/home/Footer.tsx
@@ -1,8 +1,5 @@
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-import { Mail } from "lucide-react";
 import { Linkedin, Github, Twitter } from "lucide-react";
-import Link from "next/link";
 
 export function Footer() {
   return (
